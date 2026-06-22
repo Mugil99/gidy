e78a4b4fbf18ff0f9c9580a387a5eae7f411214c
Mugil99 <mugilya99@gmail.com>
2026-06-22 20:02:06 +0530

    deplyoing frontend

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -11,11 +11,5 @@ router.get("/logs", getLogs);
 
 router.get("/dashboard-stats", getDashboardStats);
 
-router.get("/test", (req, res) => {
-  res.json({
-    success: true,
-    message: "Router Working",
-  });
-});
 
 export default router;
