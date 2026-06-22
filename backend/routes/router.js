58c9e9fd47eccc0d43b95ed5ef401c8dde6f6cc0
Mugil99 <mugilya99@gmail.com>
2026-06-22 18:51:58 +0530

    add test route

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -8,4 +8,12 @@ router.post("/upload", uploadLogs);
 router.get("/logs", getLogs);
 
 router.get("/dashboard-stats", getDashboardStats);
+
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Router Working",
+  });
+});
+
 export default router;
